856eae9ac7236a4a1970d7b88a60f624a999a451
grant-murphy <murphyg.gm@gmail.com>
2019-07-03 19:06:53 +0000

    Snake Initials

---
+++ client.js
@@ -0,0 +1,24 @@
+const net = require('net');
+
+const connect = function() {
+  const conn = net.createConnection({ 
+    host: 'localhost',
+    port: 50541
+  });
+
+  conn.on('connect', () => {
+    conn.write('Name: GJM');
+  });
+
+  conn.on('data', (data) => {
+    console.log('Server says: ', data);
+  });
+
+  // interpret incoming data as text
+  conn.setEncoding('utf8'); 
+
+  return conn;
+}
+
+
+module.exports = { connect };
