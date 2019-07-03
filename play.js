49bd37c99c68cf1849e0736d03b6858fb74d103b
grant-murphy <murphyg.gm@gmail.com>
2019-07-03 21:04:01 +0000

    UserInput & UserMessages

--- play.js
+++ play.js
@@ -1,26 +1,8 @@
 const net = require('net');
 
-
 const { connect } = require('./client');
+const { setupInput } = require('./input');
+
 console.log('Connecting ...');
-connect();
 
-const setupInput = function() {
-  const stdin = process.stdin;
-  stdin.setRawMode(true);
-  stdin.setEncoding('utf8');
-  stdin.on('data', handleUserInput)
-  stdin.resume();
-  
-  return stdin;
-}
-
-const handleUserInput = function(key) {
-  if (key === '\u0003') {
-    process.exit();
-  } 
-};
-
-setupInput();
-
-module.exports = setupInput;
+setupInput(connect());
