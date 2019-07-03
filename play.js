830d2729dc3a033bff9e238f22d5b176c7c1976b
grant-murphy <murphyg.gm@gmail.com>
2019-07-03 19:55:00 +0000

    Stdin Setup

--- play.js
+++ play.js
@@ -1,5 +1,26 @@
 const net = require('net');
 
+
 const { connect } = require('./client');
 console.log('Connecting ...');
 connect();
+
+const setupInput = function() {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding('utf8');
+  stdin.on('data', handleUserInput)
+  stdin.resume();
+  
+  return stdin;
+}
+
+const handleUserInput = function(key) {
+  if (key === '\u0003') {
+    process.exit();
+  } 
+};
+
+setupInput();
+
+module.exports = setupInput;
